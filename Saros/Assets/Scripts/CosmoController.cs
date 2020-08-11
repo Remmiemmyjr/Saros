469b4a200b9dfd8387c2b833e3ce94206825834e
Remmie <emily@isimar.com>
2020-08-11 14:59:03 -0700

    Made simple walking controls

--- Saros/Assets/Scripts/CosmoController.cs
+++ Saros/Assets/Scripts/CosmoController.cs
@@ -4,34 +4,49 @@ using UnityEngine;
 
 public class CosmoController : MonoBehaviour
 {
-    
-    public AnimationCurve curve;
+    public float speed = 4f;
+    Vector2 velocity;
+    //public AnimationCurve curve;
     Rigidbody2D rb;
-    Vector3 anchor;
+    //Vector3 anchor;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anchor = transform.position;
+        //anchor = transform.position;
     }
 
     void Update()
     {
+        velocity = rb.velocity;
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity.x = speed;
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity.x = -speed;
+        }
+        else
+        {
+            velocity.x = 0;
+        }
+        rb.velocity = velocity;
         
     }
 
-    private void OnMouseDown()
-    {
+    //private void OnMouseDown()
+    //{
 
-    }
+    //}
 
-    private void OnMouseDrag()
-    {
+    //private void OnMouseDrag()
+    //{
         
-    }
+    //}
 
-    private void OnMouseUp()
-    {
+    //private void OnMouseUp()
+    //{
         
-    }
+    //}
 }
