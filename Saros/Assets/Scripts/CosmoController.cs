fdcd4325667538ff44ef01249cec2f75529751a3
Remmie <emily@isimar.com>
2020-08-11 14:28:38 -0700

    Added the unity project and cinemachine package

---
+++ Saros/Assets/Scripts/CosmoController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CosmoController : MonoBehaviour
+{
+    
+    public AnimationCurve curve;
+    Rigidbody2D rb;
+    Vector3 anchor;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anchor = transform.position;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+
+    }
+
+    private void OnMouseDrag()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        
+    }
+}
