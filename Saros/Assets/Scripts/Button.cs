efa806f4573adcebd36c068ecc95ecb8030573d9
Eliana Musser <elianamusser@gmail.com>
2020-08-15 19:31:27 -0400

    first try at a button

---
+++ Saros/Assets/Scripts/Button.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+
+public class Button : MonoBehaviour
+{
+    public DoorOpen doorOpenScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnMouseDown()
+    {
+        UnityEngine.Debug.Log("Button clicked");
+        doorOpenScript.DoorOpening = true;
+    }
+}
---
+++ Saros/Assets/Scripts/DylanPlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DylanPlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    int speed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = new Vector2();
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement.x -= 1;
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            movement.x += 1;
+        }
+
+        rb.velocity = speed * movement;
+    }
+}
