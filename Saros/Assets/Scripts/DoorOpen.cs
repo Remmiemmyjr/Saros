fd9fe4a54c2ed55cd242c7247f6f8a8877024aed
Eliana Musser <elianamusser@gmail.com>
2020-08-15 22:20:36 -0400

    a simple button

--- Saros/Assets/Scripts/DoorOpen.cs
+++ Saros/Assets/Scripts/DoorOpen.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class DoorOpen : MonoBehaviour
 {
 
+    //Color color;
     Rigidbody2D rb;
-    Transform transform;
+    Renderer cubeRenderer;
     int doorSpeed = 4;
     public bool DoorOpening = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        //color = gameObject.GetComponent<Color>();
         rb = gameObject.GetComponent<Rigidbody2D>();
-        transform = gameObject.GetComponent<Transform>();
+        cubeRenderer = gameObject.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -23,7 +25,14 @@ public class DoorOpen : MonoBehaviour
         if(DoorOpening == true)
         {
             Debug.Log("Door opening");
-            rb.AddForce(transform.forward * doorSpeed);
+            
+            Vector2 movement = new Vector2();
+
+            movement.x += 1;
+
+            rb.velocity = movement * doorSpeed;
+
+            cubeRenderer.material.SetColor("Color_", Color.red);
         }
     }
 }
